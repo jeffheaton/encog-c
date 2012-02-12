718c5a1a40d9bdce259ca46d2f70c7db00e2441b
Jeff Heaton <jeff@jeffheaton.com>
2012-02-12 06:40:43 -0600

    Can now calculate a simple neural network error in CDUA

--- encog-cmd/cuda_eval.cu
+++ encog-cmd/cuda_eval.cu
@@ -68,7 +68,38 @@ __global__ void EncogGPULink(ENCOG_NEURAL_NETWORK *net, ENCOG_DATA *data) {
 	}	
 }
 
-__device__ void _ComputeLayer(ENCOG_NEURAL_NETWORK *net, int currentLayer)
+__device__ void EncogGPUNetworkClearContext(ENCOG_NEURAL_NETWORK *net,REAL *layerSums, REAL *layerOutput)
+{
+    INT index = 0;
+    INT hasBias;
+    INT i;
+    INT j;
+
+    for (i = 0; i < net->layerCount; i++)
+    {
+        hasBias = (net->layerContextCount[i] + net->layerFeedCounts[i]) != net->layerCounts[i];
+
+        // fill in regular neurons
+        for (j = 0; j < net->layerFeedCounts[i]; j++)
+        {
+            layerOutput[index++] = 0;
+        }
+
+        // fill in the bias
+        if (hasBias)
+        {
+            layerOutput[index++] = net->biasActivation[i];
+        }
+
+        // fill in context
+        for (j = 0; j < net->layerContextCount[i]; j++)
+        {
+            layerOutput[index++] = 0;
+        }
+    }
+}
+
+__device__ void _ComputeLayer(ENCOG_NEURAL_NETWORK *net, int currentLayer,REAL *layerSums, REAL *layerOutput)
 {
     int x;
     int y;
@@ -88,51 +119,49 @@ __device__ void _ComputeLayer(ENCOG_NEURAL_NETWORK *net, int currentLayer)
         REAL sum = 0;
         for (y = inputIndex; y < limitY; y++)
         {
-            sum += net->weights[index++] * net->layerOutput[y];
+            sum += net->weights[index++] * layerOutput[y];
         }
-        net->layerSums[x] = sum;
-        net->layerOutput[x] = sum;
+        layerSums[x] = sum;
+        layerOutput[x] = sum;
     }
 
 	EncogGPUActivationSigmoid(
-        net->layerOutput+outputIndex, outputSize);
+        layerOutput+outputIndex, outputSize);
 
     //(*net->activationFunctions[currentLayer - 1])(
     //    net->layerOutput+outputIndex, outputSize);
 }
 
-__device__ void EncogGPUNetworkCompute(ENCOG_NEURAL_NETWORK *net,REAL *input, REAL *output)
+__device__ void EncogGPUNetworkCompute(ENCOG_NEURAL_NETWORK *net,REAL *input, REAL *layerSums, REAL *layerOutput)
 {
     int i;
     int sourceIndex;
 	
 	sourceIndex = net->neuronCount - net->layerCounts[net->layerCount - 1];
 
-    memcpy(net->layerOutput+sourceIndex,input,net->inputCount*sizeof(REAL));
+    memcpy(layerOutput+sourceIndex,input,net->inputCount*sizeof(REAL));
 
     for (i = net->layerCount - 1; i > 0; i--)
     {
-        _ComputeLayer(net,i);
+        _ComputeLayer(net,i, layerSums, layerOutput);
     }
-
-	if( output!=NULL ) {
-		memcpy(output,net->layerOutput,net->outputCount*sizeof(REAL));
-	}
 }
 
 
-__global__ void EncogGPUEval(ENCOG_NEURAL_NETWORK *net, ENCOG_DATA *data, float *output)
+__global__ void EncogGPUEval(ENCOG_NEURAL_NETWORK *net, ENCOG_DATA *data, REAL *globalSums, REAL *globalOutput, float *errors)
 {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
-	REAL tmp[10];
 
     if (i < data->recordCount)
 	{
+		REAL *sums = globalSums + (i*net->neuronCount);
+		REAL *output = globalOutput + (i*net->neuronCount);
         REAL *input = EncogGPUDataGetInput(data,i);
 		REAL *ideal = EncogGPUDataGetIdeal(data,i);
-		EncogGPUNetworkCompute(net,input,tmp);
-		REAL delta = *tmp - *ideal;
-		output[i] = delta*delta;
+		EncogGPUNetworkClearContext(net, sums, output);
+		EncogGPUNetworkCompute(net,input,sums, output);
+		REAL delta = *output - *ideal;
+		errors[i] = delta*delta;
 	}
 }
 
@@ -141,13 +170,18 @@ extern "C" float EncogCUDAErrorSSE(ENCOG_NEURAL_NETWORK *net, ENCOG_DATA *data)
 {    
 	ENCOG_NEURAL_NETWORK *deviceNet; 
 	ENCOG_DATA *deviceData;
-	float *deviceResults;
-	float *results = (float*)EncogUtilAlloc(data->recordCount,sizeof(float));
+	float *deviceErrors;
+	REAL *deviceSums;
+	REAL *deviceOutput;
+	float *errors = (float*)EncogUtilAlloc(data->recordCount,sizeof(float));
 
     // Allocate vectors in device memory
     checkCudaErrors( cudaMalloc((void**)&deviceNet, net->memorySize) );
     checkCudaErrors( cudaMalloc((void**)&deviceData, data->memorySize) );
-	checkCudaErrors( cudaMalloc((void**)&deviceResults, data->recordCount * sizeof(float)) );
+	checkCudaErrors( cudaMalloc((void**)&deviceErrors, data->recordCount * sizeof(float)) );
+
+	checkCudaErrors( cudaMalloc((void**)&deviceSums, data->recordCount * net->neuronCount * sizeof(REAL)) );
+	checkCudaErrors( cudaMalloc((void**)&deviceOutput, data->recordCount * net->neuronCount * sizeof(REAL)) );
 
     // Copy vectors from host memory to device memory
     checkCudaErrors( cudaMemcpy(deviceNet, net, net->memorySize, cudaMemcpyHostToDevice) );   
@@ -159,7 +193,7 @@ extern "C" float EncogCUDAErrorSSE(ENCOG_NEURAL_NETWORK *net, ENCOG_DATA *data)
     int threadsPerBlock = 256;
     int blocksPerGrid = (data->recordCount + threadsPerBlock - 1) / threadsPerBlock;
    
-	EncogGPUEval<<<blocksPerGrid, threadsPerBlock>>>(deviceNet,deviceData,deviceResults);
+	EncogGPUEval<<<blocksPerGrid, threadsPerBlock>>>(deviceNet,deviceData,deviceSums,deviceOutput,deviceErrors);
     
 	getLastCudaError("kernel launch failure");
 #ifdef _DEBUG
@@ -168,19 +202,32 @@ extern "C" float EncogCUDAErrorSSE(ENCOG_NEURAL_NETWORK *net, ENCOG_DATA *data)
 
     // Copy result from device memory to host memory
     // h_C contains the result in host memory
-    checkCudaErrors( cudaMemcpy(results, deviceResults, data->recordCount * sizeof(float), cudaMemcpyDeviceToHost) );
+    checkCudaErrors( cudaMemcpy(errors, deviceErrors, data->recordCount * sizeof(float), cudaMemcpyDeviceToHost) );
+
+	REAL *temp = (REAL*)EncogUtilAlloc(data->recordCount * net->neuronCount ,sizeof(REAL));
+	checkCudaErrors( cudaMemcpy(temp, deviceOutput, data->recordCount * net->neuronCount * sizeof(REAL), cudaMemcpyDeviceToHost) );
 
     cudaFree(deviceData);
 	cudaFree(deviceNet);
-	cudaFree(deviceResults);
+	cudaFree(deviceErrors);
+	cudaFree(deviceSums);
+	cudaFree(deviceOutput);
 #if (CUDA_VERSION > 4010 )        
     cudaDeviceReset();
 #endif	
 
 	float sum = 0;
 	for(int i=0;i<data->recordCount;i++) {
-	printf("%f\n",results[i]);
-		sum+=results[i];
+		
+		REAL *ptr = temp + (i*net->neuronCount);
+		for(int j=0;j<net->neuronCount;j++) {
+			printf("%f ",(float)*ptr);
+			ptr++; 
+		}
+		printf("\n");
+		//printf("\n%f\n",errors[i]);
+	
+		sum+=errors[i];
 	}
 
 	return sum/data->recordCount;   
